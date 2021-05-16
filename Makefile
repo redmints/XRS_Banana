62368898215ad13c40437f3bb3467c3e2b8c0f3b
Mathias HANNA <mathias.hanna3@gmail.com>
2021-05-16 19:47:19 +0200

    Remove docker API (placed in another git repo)

--- Makefile
+++ Makefile
@@ -14,9 +14,6 @@ ${OUT_DIR}:
 	${MKDIR_P} ${OUT_DIR}
 build:
 	g++ -c ${SRC_DIR}/Utils.cpp -o ${OUT_DIR}/Utils.o
-	g++ -c ${SRC_DIR}/docker/Image.cpp -o ${OUT_DIR}/Image.o
-	g++ -c ${SRC_DIR}/docker/Container.cpp -o ${OUT_DIR}/Container.o
-	g++ -c ${SRC_DIR}/docker/Docker.cpp -o ${OUT_DIR}/Docker.o
 	g++ -std=c++11 -c ${SRC_DIR}/controllers/accueil.cpp -o ${OUT_DIR}/accueil.o
 	g++ -std=c++11 -lpthread -g -Wall -o ${OUT_DIR}/${BINARY} ${OUT_DIR}/*.o ${SRC_DIR}/main.cpp
 docu:
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,14 @@
 
 #include <iostream>
-#include "docker/Docker.hpp"
-#include <vector>
 #include "../include/cpp-httplib/httplib.h"
+#include "Utils.hpp"
 
 #include "controllers/accueil.hpp"
 
 using namespace std;
 
-void list_containers()
-{
-    vector<Container> containers = Docker::list_containers();
-    for(long unsigned int i = 0; i < containers.size(); i++)
-    {
-        printf("%s %s\n", containers[i].get_id().c_str(), containers[i].get_name().c_str());
-    }
-}
-
-void list_images()
-{
-    vector<Image> images = Docker::list_images();
-    for(long unsigned int i = 0; i < images.size(); i++)
-    {
-        printf("%s %s\n", images[i].get_id().c_str(), images[i].get_repository().c_str());
-    }
-}
-
 int main(int argc, const char * argv[])
 {
-    //list_containers();
-    //list_images();
-
     httplib::Server svr;
 
     svr.Get("/", [](const httplib::Request &req, httplib::Response &res)
