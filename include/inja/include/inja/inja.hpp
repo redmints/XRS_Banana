32a893ba9d3629aad6faaf49b594777f4ec2d2a6
Mathias HANNA <mathias.hanna3@gmail.com>
2021-08-10 00:06:49 +0200

    Added dependancies

---
+++ include/inja/include/inja/inja.hpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2020 Pantor. All rights reserved.
+
+#ifndef INCLUDE_INJA_INJA_HPP_
+#define INCLUDE_INJA_INJA_HPP_
+
+#include <nlohmann/json.hpp>
+
+#if (defined(__cpp_exceptions) || defined(__EXCEPTIONS) || defined(_CPPUNWIND)) && !defined(INJA_NOEXCEPTION)
+    #define INJA_THROW(exception) throw exception
+#else
+    #include <cstdlib>
+    #define INJA_THROW(exception) std::abort()
+#endif
+
+#include "environment.hpp"
+#include "exceptions.hpp"
+#include "parser.hpp"
+#include "renderer.hpp"
+#include "string_view.hpp"
+#include "template.hpp"
+
+#endif // INCLUDE_INJA_INJA_HPP_
